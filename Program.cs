69cd769c5b2aea9b948fd0a718328cdd0b304588
Ovestt <radikmurtazin@yahoo.com>
2025-06-14 01:57:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите градусы угла: ");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите угловые минуты: ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите угловые секунды: ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            Corner corner = new Corner(gradus, min, sec);
+            Console.Write(corner.ToRadius());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Сorner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10
+{
+    class Corner
+    {
+        decimal gradus;
+        decimal min;
+        int sec;
+        public decimal Gradus
+        {
+            set
+            {
+                gradus = value % 360;
+            }
+            get
+            {
+                return gradus;
+            }
+        }
+        public decimal Min
+        {
+            set
+            {
+                Gradus += value / 60;
+            }
+            get
+            {
+                return min;
+            }
+        }
+        public int Sec
+        {
+            set
+            {
+                Gradus += value / 3600;
+            }
+            get
+            {
+                return sec;
+            }
+        }
+        public Corner(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+        public decimal ToRadius()
+        {
+            return Convert.ToDecimal(Math.PI) * gradus / 180;
+        }
+    }
+}
